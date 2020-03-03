8d55690b25dfc10e439d39414639d5c132ca3fea
Kyle Lo <36239201+lo7kyle@users.noreply.github.com>
2020-03-02 20:19:57 -0800

    Add files via upload

---
+++ Twitter_Sentiment.py
@@ -0,0 +1,4 @@
+from sklearn import tree
+
+#[height, weight, shoe size]
+x = [[181,80,44], [177, 70, 43] ]
---
+++ python3tutorial.py
@@ -0,0 +1,12 @@
+print("  /|")
+print(" / |")
+print("/  |")
+print("_ _")
+print("hello world")
+print("hello world")
+print("hello world")
+programming_languages = "Python", "Java", "C++", "C#"
+
+for lang in programming_languages: 
+    print(lang)
+    
